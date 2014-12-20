e7415768b66537acb86757954de218a03f7422ae
Greg Osuri <gosuri@gmail.com>
2014-12-19 16:38:50 -0800

    Bumping the version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'gosuri@demandbase.com'
 license          'MIT'
 description      'The cookbook includes recipe and LWRP to move a directory in a partition to a new aws ebs volume'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 recipe           'ebs-directory', 'Installs the right_aws gem during compile time'
 
 depends 'aws', '2.5.0'
